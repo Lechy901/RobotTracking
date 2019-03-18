f75e6db3bec9a86d8ad55618214705211ba804b7
Adam Lechovský <lechy901@gmail.com>
2019-03-18 19:24:00 +0100

    changes to graph detection

--- src/base/GraphFinder.java
+++ src/base/GraphFinder.java
@@ -3,7 +3,15 @@ package base;
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 import org.bytedeco.javacpp.indexer.Indexer;
+
+import util.Direction;
+import util.Utils;
 
 public class GraphFinder {
 	public void findGraph(Mat input) {
@@ -24,24 +32,120 @@ public class GraphFinder {
 		Canny(dilated, edges, 50, 150);
 		Utils.display(edges, "edges");
 		
-		int rho = 1;
-		double theta = Math.PI / 180;
-		int threshold = 10;
-		int min_line_len = 250;
-		int max_line_gap = 10;
-		Mat line_img = new Mat(scaled);
-		
-		Mat lines = new Mat();
-		HoughLinesP(edges, lines, rho, theta,threshold, min_line_len, max_line_gap);
-		System.out.println(lines);
-		Indexer indexer = lines.createIndexer();
-		for(int i = 0; i < indexer.rows(); i++) {
-			line(line_img, new Point((int)indexer.getDouble(0, i, 0), (int)indexer.getDouble(0, i, 1)),
-					       new Point((int)indexer.getDouble(0, i, 2), (int)indexer.getDouble(0, i, 3)),
-					       new Scalar(255, 0, 0, 255));
+		final int reach = 15;
+		Indexer edgeIndexer = edges.createIndexer();
+		for(int i = reach; i < edgeIndexer.cols() - reach; i++) {
+			for(int j = reach; j < edgeIndexer.rows() - reach; j++) {
+				int white_count = 0;
+				boolean left = false, up = false, right = false, down = false;
+				for(int k = 0; k < reach; k++) {
+					if(edgeIndexer.getDouble(j-k, i, 0) > 200) {
+						white_count++;
+						left = true;
+						break;
+					}
+				}
+				for(int k = 0; k < reach; k++) {
+					if(edgeIndexer.getDouble(j+k, i, 0) > 200) {
+						white_count++;
+						right = true;
+						break;
+					}
+				}
+				for(int k = 0; k < reach; k++) {
+					if(edgeIndexer.getDouble(j, i+k, 0) > 200) {
+						white_count++;
+						down = true;
+						break;
+					}					
+				}
+				for(int k = 0; k < reach; k++) {
+					if(edgeIndexer.getDouble(j, i-k, 0) > 200) {
+						white_count++;
+						up = true;
+						break;
+					}
+					
+				}
+				
+				if (white_count == 3) {
+					if (!left) {
+						edgeIndexer.putDouble(new long[] {j, i, 0}, 125.0 );
+					} else if (!up) {
+						edgeIndexer.putDouble(new long[] {j, i, 0}, 126.0 );
+					} else if (!right) {
+						edgeIndexer.putDouble(new long[] {j, i, 0}, 127.0 );
+					} else {
+						edgeIndexer.putDouble(new long[] {j, i, 0}, 128.0 );
+					}
+					
+				}
+			}
 		}
 		
-		Utils.display(line_img, "lines");
+		List<EndVertex> endVertices = new ArrayList<EndVertex>();
+		final int edgeSquareSide = 6;
+		for(int i = reach; i < edgeIndexer.cols() - reach; i++) {
+			for(int j = reach; j < edgeIndexer.rows() - reach; j++) {
+				if (edgeIndexer.getDouble(j, i, 0) > 124 && edgeIndexer.getDouble(j, i, 0) < 129) {
+					boolean isGraySquare = true;
+					for(int i2 = 0; i2 < edgeSquareSide; i2++) {
+						for(int j2 = 0; j2 < edgeSquareSide; j2++) {
+							if (edgeIndexer.getDouble(j + j2,i + i2,0) < 124 || edgeIndexer.getDouble(j + j2, i + i2, 0) > 129) {
+								isGraySquare = false;
+							}
+						}
+					}
+					if (isGraySquare) {
+						double squarePixel = edgeIndexer.getDouble(j + (edgeSquareSide / 2), i + (edgeSquareSide / 2), 0);
+						if (squarePixel < 125.5) {
+							endVertices.add(new EndVertex(new Point(j + (edgeSquareSide / 2), i + (edgeSquareSide / 2)), Direction.LEFT));
+						} else if (squarePixel < 126.5) {
+							endVertices.add(new EndVertex(new Point(j + (edgeSquareSide / 2), i + (edgeSquareSide / 2)), Direction.UP));
+						} else if (squarePixel < 127.5) {
+							endVertices.add(new EndVertex(new Point(j + (edgeSquareSide / 2), i + (edgeSquareSide / 2)), Direction.RIGHT));
+						} else {
+							endVertices.add(new EndVertex(new Point(j + (edgeSquareSide / 2), i + (edgeSquareSide / 2)), Direction.DOWN));
+						}
+						
+						
+						// BFS to color neighboring gray pixels black
+						Queue<Integer> q = new LinkedList<Integer>();
+						q.add(j);
+						q.add(i);
+						while(!q.isEmpty()) {
+							int x = q.remove();
+							int y = q.remove();
+							edgeIndexer.putDouble(new long[] {x, y, 0}, 0.0);
+							double neighborPixel = edgeIndexer.getDouble(x - 1, y, 0);
+							if (neighborPixel > 124 && neighborPixel < 129) {
+								q.add(x - 1);
+								q.add(y);
+							}
+							neighborPixel = edgeIndexer.getDouble(x + 1, y, 0);
+							if (neighborPixel > 124 && neighborPixel < 129) {
+								q.add(x + 1);
+								q.add(y);
+							}
+							neighborPixel = edgeIndexer.getDouble(x, y - 1, 0);
+							if (neighborPixel > 124 && neighborPixel < 129) {
+								q.add(x);
+								q.add(y - 1);
+							}
+							neighborPixel = edgeIndexer.getDouble(x, y + 1, 0);
+							if (neighborPixel > 124 && neighborPixel < 129) {
+								q.add(x);
+								q.add(y + 1);
+							}
+						}
+					}
+				}
+			}	
+		}
+		
+		Utils.display(edges, "graphDetected");
+		System.out.println(endVertices.size());
+		ImageGraph ig = new ImageGraph(endVertices);
 	}
 	
 }
--- src/main/TrackingMain.java
+++ src/main/TrackingMain.java
@@ -1,46 +1,57 @@
 package main;
 
 import org.bytedeco.javacv.*;
+
+import base.*;
+import util.Utils;
+
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
 import static org.bytedeco.javacpp.opencv_videoio.*;
+import static org.bytedeco.javacpp.opencv_imgcodecs.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.*;
 
 public class TrackingMain {
 
 	public static void main(String[] args) {
 		
+		boolean loadFromStorage = true;
 		System.out.println("hello");
 		
 		Mat frame = new Mat();
+		VideoCapture vc = null;
 		
-		VideoCapture vc = new VideoCapture();
-		vc.open(0);
-		if (!vc.isOpened()) {
-			System.err.println("VideoCapture is not opened");
-			vc.close();
-			return;
-		}
-		System.out.println("VideoCapture opened successfully");
-		System.out.println("Starting to grab frames");
-		for (int i = 0; i < 5; i++) {
-			vc.read(frame);
-			if (frame.empty()) {
-				System.out.println("empty frame grabbed wtf");
-				continue;
+		if (loadFromStorage) {
+			frame = imread("testframe.png");
+		} else {
+			vc = new VideoCapture();
+			vc.open(1);
+			if (!vc.isOpened()) {
+				System.err.println("VideoCapture is not opened");
+				vc.close();
+				return;
 			}
-			System.out.println("frame grabbed successfully");
-			//break;
+			System.out.println("VideoCapture opened successfully");
+			System.out.println("Starting to grab frames");
+			for (int i = 0; i < 5; i++) {
+				vc.read(frame);
+				if (frame.empty()) {
+					System.out.println("empty frame grabbed wtf");
+					continue;
+				}
+				System.out.println("frame grabbed successfully");
+				//break;
+			}
 		}
-
+		Utils.display(frame, "new webcam frame");
+		System.out.println(frame);
 		GraphFinder gf = new GraphFinder();
 		gf.findGraph(frame);
 		
-		vc.close();
+		if (!loadFromStorage)
+			vc.close();
 	}
 
 }
